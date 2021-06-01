e922eacc5a103026b906cd98c65ef8f42101b59e
jhlov <jhlovv@gmail.com>
2021-06-01 18:32:38 +0900

    모바일 용 container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import classNames from "classnames";
 import React from "react";
 import "./App.scss";
 
 function App() {
+  const isMobile = false;
+
   return (
     <div className="App">
-      <div className="container"></div>
+      <div
+        className={classNames(isMobile ? "mobile-container" : "container")}
+      ></div>
     </div>
   );
 }
